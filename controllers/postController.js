8e7cff3a89c4ae259276812a61dddce140d72f0d
RobertoFloris <florisroberto99@gmail.com>
2025-02-05 17:32:13 +0100

    [fix] index in postController.js

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,7 +1,13 @@
 const connection = require('../data/db')
 
 const index = (req, res) => {
-  res.send("Elenco post")
+  const sql = 'SELECT * FROM posts'
+  connection.query(sql, (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: "Query errata" })
+    }
+    res.json(results)
+  })
 }
 
 const show = (req, res) => {
